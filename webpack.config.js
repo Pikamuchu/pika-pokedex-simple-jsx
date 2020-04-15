99f9fd3075af45391535d78f6b096fea4529b28f
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2020-04-15 02:51:13 +0200

    webpack client config improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,54 @@
 const webpack = require('webpack');
+const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env = {}, argv) => {
-  const isDevelopment = argv.mode === 'development'
+  const isDevelopment = argv.mode === 'development';
+
   let config = {
-    entry: './src/client/index.js',
+    entry: ['./src/client/index.js', './src/client/styles/main.scss'],
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/,
+          test: /\.js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader'
           }
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          exclude: /node_modules/,
+          use: ['file-loader']
         }
       ]
     },
     output: {
       path: __dirname + '/dist',
-      publicPath: '/',
-      filename: 'bundle.js'
-    }
+      publicPath: '/'
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'main.css'
+      })
+    ]
   };
 
   if (isDevelopment) {
-    config.output.path = __dirname + '/public',
-    config.plugins = [new webpack.HotModuleReplacementPlugin]
+    config.devtool = 'source-map';
+    config.output.path = __dirname + '/public';
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
     config.devServer = {
       contentBase: './public',
       hot: true
     };
+  } else {
+    config.plugins.push(new uglifyJsPlugin());
   }
 
   return config;
