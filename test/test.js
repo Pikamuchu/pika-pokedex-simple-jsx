515a4cea1396f8395a13602f5bf8f621b933bb5d
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2020-06-15 13:23:07 +0200

    fix test

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ import server from '../src/server/index.js';
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:3000', res => {
+    http.get('http://127.0.0.1:3000/pokemons', res => {
       assert.equal(200, res.statusCode);
       server.close();
       done();
